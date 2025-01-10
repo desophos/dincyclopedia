821201b79e975887c7f6782968ee3ff49a633620
desophos <1887450+desophos@users.noreply.github.com>
2025-01-10 04:18:57 -0600

    Add Path extension methods

--- parser/src/util.scala
+++ parser/src/util.scala
@@ -1,1 +1,10 @@
 package dincyclopedia.parser
+
+import os.Path
+import os.SubPath
+
+extension (path: Path) {
+  def dropLast: Path = os.root / path.segments.toList.init
+  def contains(sub: SubPath): Boolean =
+    path.segments.sliding(sub.segments.size).contains(sub.segments)
+}
