c04554ce79e77953fff165c88868da86419cdfce
desophos <1887450+desophos@users.noreply.github.com>
2025-02-03 07:38:31 -0600

    Arbitrary and Gen values for parser tests

---
+++ parser/test/src/arbitrary.scala
@@ -0,0 +1,43 @@
+package dincyclopedia.parser.test
+
+import dincyclopedia.parser.ParsedEntry
+
+import cats.data.NonEmptyList
+import cats.implicits.*
+import io.github.martinhh.derived.scalacheck.deriveArbitrary
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen
+
+given Arbitrary[ParsedEntry]               = deriveArbitrary
+given Arbitrary[NonEmptyList[ParsedEntry]] = deriveArbitrary
+
+def uniqueListOfN[T](n: Int, g: Gen[T]): Gen[List[T]] = {
+
+  def nonDuplicates[T](existing: List[T], toAdd: LazyList[T]): List[T] = {
+    if existing.length >= n then existing
+    else {
+      val nextToAdd = toAdd.dropWhile(existing.contains)
+      nonDuplicates(existing :+ nextToAdd.head, nextToAdd.tail)
+    }
+  }
+
+  Gen.infiniteLazyList(g).map(nonDuplicates(List.empty, _))
+}
+
+def uniqueNelOfN[T](n: Int, g: Gen[T]): Gen[NonEmptyList[T]] = for {
+  t  <- g
+  ts <- uniqueListOfN(n - 1, g)
+} yield NonEmptyList(t, ts)
+
+val sameTitleEntries = for {
+  e     <- Arbitrary.arbitrary[ParsedEntry]
+  es    <- Gen.nonEmptyListOf(Arbitrary.arbitrary[ParsedEntry])
+  title <- Gen.nonEmptyStringOf(Gen.asciiChar)
+} yield NonEmptyList(e, es).map(_.copy(title = title, parent = None))
+
+val differentTitleEntries = for {
+  e  <- Arbitrary.arbitrary[ParsedEntry]
+  es <- Gen.nonEmptyListOf(Arbitrary.arbitrary[ParsedEntry])
+  nel = NonEmptyList(e, es)
+  titles <- uniqueNelOfN(nel.length, Gen.nonEmptyStringOf(Gen.asciiChar))
+} yield nel.zipWith(titles)((e, title) => e.copy(title = title, parent = None))
