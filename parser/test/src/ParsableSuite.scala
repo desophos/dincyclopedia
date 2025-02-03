27db3fba2348ef2c3e7327c034097a64c5e69900
desophos <1887450+desophos@users.noreply.github.com>
2025-02-03 07:38:56 -0600

    Tests for `groupEntriesByTitle`

---
+++ parser/test/src/ParsableSuite.scala
@@ -0,0 +1,35 @@
+package dincyclopedia.parser
+package test
+
+import scala.collection.MapView
+
+import cats.data.NonEmptyList
+import munit.ScalaCheckSuite
+import org.scalacheck.Prop.*
+
+class ParsableSuite extends ScalaCheckSuite {
+  property("groupEntriesByTitle when all entries have the same title") {
+    forAll(sameTitleEntries) { (entries: NonEmptyList[ParsedEntry]) =>
+      val groupedEntries: MapView[String, NonEmptyList[Map[String, String]]] =
+        Parsable.groupEntriesByTitle(entries)
+
+      assert(groupedEntries.nonEmpty)
+      assertEquals(1, groupedEntries.keys.size)
+      assertEquals(entries.length, groupedEntries.values.map(_.length).sum)
+    }
+  }
+
+  property("groupEntriesByTitle when all entries have different titles") {
+    forAll(differentTitleEntries) { (entries: NonEmptyList[ParsedEntry]) =>
+      val groupedEntries: MapView[String, NonEmptyList[Map[String, String]]] =
+        Parsable.groupEntriesByTitle(entries)
+
+      assert(groupedEntries.nonEmpty)
+      assertEquals(entries.length, groupedEntries.keys.size)
+      groupedEntries.values.tapEach(entryList =>
+        assertEquals(1, entryList.length)
+      )
+      assertEquals(entries.length, groupedEntries.values.map(_.length).sum)
+    }
+  }
+}
