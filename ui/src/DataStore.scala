d1250545e38fd2503be1d98feb95a1fe8cd928e8
desophos <1887450+desophos@users.noreply.github.com>
2025-02-03 07:41:44 -0600

    DataStore fns should take baseUrl as a parameter for testability and separation of concerns

--- ui/src/DataStore.scala
+++ ui/src/DataStore.scala
@@ -5,6 +5,7 @@ import dincyclopedia.model.given
 
 import calico.*
 import calico.html.io.*
+import cats.Show
 import cats.data.OptionT
 import cats.effect.*
 import io.circe.Decoder
@@ -15,24 +16,25 @@ import org.scalajs.dom.URL
 class DataStore(val magicModifiers: Map[String, LeveledMagicModifier])
 
 object DataStore {
-  def makeUrl[A: JsonStorage]: URL =
-    URL(f"${JsonStorage[A].filename}.json", DincyclopediaApp.BASE_URL)
+  private def makeUrl[A: JsonStorage](baseUrl: String): URL =
+    URL(f"${JsonStorage[A].filename}.json", baseUrl)
 
-  def getData[A <: Entry: Decoder: JsonStorage](using
+  private def getData[A <: Entry: Decoder: JsonStorage](baseUrl: String)(using
       Logger[IO]
   ): OptionT[IO, Map[String, A]] =
-    fetchResponseJson(makeUrl[A].toString)
+    fetchResponseJson(makeUrl[A](baseUrl).toString)
       .flatMap(_.as[Map[String, A]].logLeftToOption)
       .flatTapNone(
         Logger[IO].error(f"Couldn't get ${JsonStorage[A].filename} data")
       )
 
-  def apply(using Logger[IO]): Resource[IO, DataStore] = (for {
-    magicModifiers <- getData[MagicModifier]
+  def apply(baseUrl: String)(using Logger[IO]): Resource[IO, DataStore] = (for {
+    magicModifiers <- getData[MagicModifier](baseUrl)
     leveledMagicModifiers = magicModifiers
       .map { (name, value) => value.leveled.map(_.name).zip(value.leveled) }
       .flatten
       .toMap
+    _ <- OptionT.liftF(debugTrace(leveledMagicModifiers))
   } yield new DataStore(leveledMagicModifiers))
     .getOrRaise(new RuntimeException("Failed to construct DataStore"))
     .toResource
